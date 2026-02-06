4d5e3651bddd7540e88be166f13ad9def0a0d32f
Montinou <agusmontoya@gmail.com>
2026-02-06 18:20:36 -0300

    🐛 Fix: tailwind.config darkMode type

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,6 +1,6 @@
 import type { Config } from 'tailwindcss';
 const config: Config = {
-  darkMode: ['class'],
+  darkMode: 'class',
   content: ['./src/**/*.{js,ts,jsx,tsx,mdx}'],
   theme: {
     extend: {
